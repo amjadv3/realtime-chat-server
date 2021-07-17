8318f345e293ddb43413c5c675e311017d4d2098
amjadv3 <mhdamjad5785@gmail.com>
2021-07-17 19:54:58 +0530

    server updat

---
+++ users.js
@@ -0,0 +1,31 @@
+const users = [];
+
+const addUser = ({ id, name, room }) => {
+	name = name.trim().toLowerCase;
+	room = room.trim().toLowerCase;
+
+	const existingUser = users.find(
+		(users) => users.name === name && users.room === room
+	);
+	if (existingUser) {
+		return { error: 'UserName is taken' };
+	}
+	const user = { id, name, room };
+	users.push(user);
+
+	return { user };
+};
+
+const removeUser = ({ id }) => {
+	const index = users.findIndex((users) => users.id === id);
+
+	if (index !== -1) {
+		return users.splice(index, 1)[0];
+	}
+};
+
+const getUser = (id) => users.find((users) => users.id === id);
+
+const getUsersInRoom = (room) => users.filter((users) => users.room === room);
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
