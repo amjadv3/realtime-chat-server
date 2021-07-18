2e525542c67849ce266f1bd2553ffff021c8f2b8
Mohammed Amjad <65709425+amjadv3@users.noreply.github.com>
2021-07-18 17:12:09 +0530

    Update users.js

--- users.js
+++ users.js
@@ -1,8 +1,8 @@
 const users = [];
 
 const addUser = ({ id, name, room }) => {
-	name = name.trim().toLowerCase;
-	room = room.trim().toLowerCase;
+	name = name.trim().toLowerCase();
+	room = room.trim().toLowerCase();
 
 	const existingUser = users.find(
 		(users) => users.name === name && users.room === room
